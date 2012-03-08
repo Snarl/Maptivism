acbc2d28bc34d6c86b5d9c01a72f023088563faa
Stephen Caraher <moskvax@gmail.com>
2012-03-08 07:38:42 +0000

    finished refactoring maptivism.js, it should work for a reason now

--- js/maptivism.js
+++ js/maptivism.js
@@ -14,54 +14,25 @@ function init_map() {
 			draggable: true
 		}
 	);
-/*	HALF FINISHED REFACTORING by stephen
-	var msids = [	'212377250988774351922.0004b6224ba561ebee1b0', /* "Occupations.kml" 
-			'212377250988774351922.0004ba74e92f1c118c517', /* "Free Wifi  Electricity.kml" 
-			'212377250988774351922.0004ba7516a1ac1e05ac9', /* "Skipping  Food.kml"
-			'212377250988774351922.0004ba75119e1444481ff', /* "Toilets.kml" 
-			'212377250988774351922.0004ba7504886c2262b6a'] /* "Showers.kml"
-/* sotiri's advice: view-source:http://wind.wiran.gr/?page=pickup&subpage=gmap&object_lat=form_node.elements%5B%27nodes__latitude%27%5D&object_lon=form_node.elements%5B%27nodes__longitude%27%5D 
-	msids.forEach(google.maps.KmlLayer('http://maps.google.co.uk/maps/ms?authuser=0&hl=en&ie=UTF8&msa=0&output=kml&msid=' 
-8=
-*/
-	mapviewlayer = new google.maps.KmlLayer('http://maps.google.co.uk/maps/ms?authuser=0&vps=5&hl=en&ie=UTF8&msa=0&output=kml&msid=212377250988774351922.0004b6224ba561ebee1b0', 
-		{
-			map: map,
-			preserveViewport: true,
-			clickable: true
-		}
-	);
-	mapviewlayer = new google.maps.KmlLayer('http://maps.google.co.uk/maps/ms?authuser=0&vps=3&hl=en&ie=UTF8&msa=0&output=kml&msid=212377250988774351922.0004ba74e92f1c118c517', /* "Free Wifi  Electricity.kml" */
-		{
-			map: map,
-			preserveViewport: true,
-			clickable: true
-		}
-	);
+
+	var msids = [	'212377250988774351922.0004b6224ba561ebee1b0', /* "Occupations.kml" */
+			'212377250988774351922.0004ba74e92f1c118c517', /* "Free Wifi  Electricity.kml" */ 
+			'212377250988774351922.0004ba7516a1ac1e05ac9', /* "Skipping  Food.kml" */
+			'212377250988774351922.0004ba75119e1444481ff', /* "Toilets.kml"  */
+			'212377250988774351922.0004ba7504886c2262b6a'] /* "Showers.kml" */
+/* sotiri's advice: view-source:http://wind.wiran.gr/?page=pickup&subpage=gmap&object_lat=form_node.elements%5B%27nodes__latitude%27%5D&object_lon=form_node.elements%5B%27nodes__longitude%27%5D  */
+
+	var kml_layers = [];
+	msids.forEach(function(msid) {  + }, msid )
 	
-	mapviewlayer = new google.maps.KmlLayer('http://maps.google.co.uk/maps/ms?authuser=0&vps=6&hl=en&ie=UTF8&msa=0&output=kml&msid=212377250988774351922.0004ba7516a1ac1e05ac9', /* "Skipping  Food.kml"*/
-		{
-			map: map,
-			preserveViewport: true,
-			clickable: true
-		}
-	);
-	
-	mapviewlayer = new google.maps.KmlLayer('http://maps.google.co.uk/maps/ms?authuser=0&vps=2&hl=en&ie=UTF8&msa=0&output=kml&msid=212377250988774351922.0004ba75119e1444481ff', /* "Toilets.kml" */
-		{
-			map: map,
-			preserveViewport: true,
-			clickable: true
-		}
-	);
-	
-	mapviewlayer = new google.maps.KmlLayer('http://maps.google.co.uk/maps/ms?authuser=0&vps=7&hl=en&ie=UTF8&msa=0&output=kml&msid=212377250988774351922.0004ba7504886c2262b6a', /* "Showers.kml" */
-		{
-			map: map,
-			preserveViewport: true,
-			clickable: true
-		}
-	);												
+	for (msid in msids) {
+		kml_layers.push(new google.maps.KmlLayer('http://maps.google.co.uk/maps/ms?authuser=0&hl=en&ie=UTF8&msa=0&output=kml&msid=' + msid',
+								{ map: map,
+								  preserveViewport: true,
+								  clickable: true }));
+	for (l in kml_layers) {
+		l.setMap(map);
+	}
 	
 	if (navigator.geolocation) {
 		navigator.geolocation.watchPosition(
